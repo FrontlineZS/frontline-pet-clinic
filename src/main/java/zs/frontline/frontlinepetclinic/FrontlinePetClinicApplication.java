019ac4e96299343464e27c7b78db92a01a62cf33
radoslaw.kochaniuk <radoslaw.kochaniuk@assecods.pl>
2019-01-03 10:23:15 +0100

    Initial Project Add

---
+++ src/main/java/zs/frontline/frontlinepetclinic/FrontlinePetClinicApplication.java
@@ -0,0 +1,14 @@
+package zs.frontline.frontlinepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontlinePetClinicApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FrontlinePetClinicApplication.class, args);
+	}
+}
+
