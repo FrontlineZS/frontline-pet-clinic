571a64d6c9ec32c3f6e9a6463d56d806c2d92012
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-01-28 17:47:19 +0100

    just for test unhooked issue. Closes #2.

--- src/main/java/zs/frontline/frontlinepetclinic/FrontlinePetClinicApplication.java
+++ src/main/java/zs/frontline/frontlinepetclinic/FrontlinePetClinicApplication.java
@@ -3,6 +3,7 @@ package zs.frontline.frontlinepetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FrontlinePetClinicApplication {
 
