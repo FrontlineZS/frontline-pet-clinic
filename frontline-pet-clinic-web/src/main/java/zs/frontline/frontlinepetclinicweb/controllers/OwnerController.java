3a327a2496d2205117ca98bd0c62ba80cbe037e7
Radosław Kochaniuk <radoslaw.kochaniuk@asssecods.pl>
2019-03-28 07:15:54 +0100

    Owner Index and Controller__closes #13

--- frontline-pet-clinic-web/src/main/java/zs/frontline/frontlinepetclinicweb/controllers/OwnerController.java
+++ frontline-pet-clinic-web/src/main/java/zs/frontline/frontlinepetclinicweb/controllers/OwnerController.java
@@ -1,17 +1,16 @@
 package zs.frontline.frontlinepetclinicweb.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
-
+public class OwnerController {
+    
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
-        return "vets/index";
+    public String listOwners() {
+        return "owners/index";
     }
 }
